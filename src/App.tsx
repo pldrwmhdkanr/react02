08536e16f26d64559a1f75ffc0e068a63e2f6ad6
Xueyanlei <1263510308@qq.com>
2026-01-13 09:08:28 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold underline">hello world</h1>
+    </div>
+  )
+}
+
+export default App
